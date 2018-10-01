64d18d1e3ac4ccf7b6321662e03f941453de9a43
yingying <chen.yingying.01988@gmail.com>
2018-10-01 16:08:46 +0100

    Tests passed getter functions in Manager Class

--- src/main/java/Staff/Management/Manager.java
+++ src/main/java/Staff/Management/Manager.java
@@ -1,5 +1,16 @@
 package Staff.Management;
 
-public class Manager {
+import Staff.Employee;
 
+public class Manager extends Employee {
+    private String deptName;
+
+    public Manager(String name, String NI_number, Double salary, String deptName) {
+        super(name, NI_number, salary);
+        this.deptName = deptName;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
 }
--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,36 @@
+import Staff.Management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class ManagerTest {
+    Manager manager;
+
+    @Before
+    public void setUp() {
+        manager = new Manager("Bob", "DF000000", 2000.0, "Glasgow");
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("Bob", manager.getName());
+    }
+
+    @Test
+    public void getNInumber() {
+        assertEquals("DF000000", manager.getNI_number());
+    }
+
+    @Test
+    public void getSalary() {
+        assertEquals(2000.0,manager.getSalary());
+    }
+
+    @Test
+    public void getDeptName() {
+        assertEquals("Glasgow", manager.getDeptName());
+    }
+
+
 }
